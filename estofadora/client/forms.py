f9fb41836df7a65a539d8a7a1323850ae8a949c8
delete <pinheiro.llip@gmail.com>
2015-01-19 12:01:45 -0200

    client form created

---
+++ estofadora/client/forms.py
@@ -0,0 +1,9 @@
+from django import forms
+from .models import Client
+
+
+class ClientForm(forms.ModelForm):
+
+	class Meta:
+		model = Client
+		fields = ['name', 'adress', 'email', 'telephone1', 'telephone2']
