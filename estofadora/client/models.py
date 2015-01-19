98a12a22f06aa1449aae2e7d4af7a6f45f02f154
delete <pinheiro.llip@gmail.com>
2015-01-19 12:01:16 -0200

    client model created

--- estofadora/client/models.py
+++ estofadora/client/models.py
@@ -1,3 +1,12 @@
 from django.db import models
 
-# Create your models here.
+
+class Client(models.Model):
+
+	name = models.CharField('Nome', max_length=200, null=False, blank=False)
+	email = models.EmailField('Email', unique=True, blank=True)
+	adress = models.CharField(u'Endereço', max_length=256, null=False, blank=False)
+	telephone1 = models.CharField('Telefone 1', max_length=15, blank=False)
+	telephone2 = models.CharField('Telefone 2', max_length=15, blank=True)
+	is_active = models.BooleanField(u'Está ativo?', default=True)
+	date_join = models.DateTimeField('Data de cadastro', auto_now_add=True)
