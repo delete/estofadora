a6be40e6473292a17806b95b4ab92b77eded5e65
delete <pinheiro.llip@gmail.com>
2015-01-20 10:38:50 -0200

    tests to clientForm was added

--- estofadora/client/tests/__init__.py
+++ estofadora/client/tests/__init__.py
@@ -1,6 +1,8 @@
 from django.test import TestCase
 from django.test.client import Client
 
+from estofadora.client.forms import ClientForm
+
 
 def create_client(**kwargs):
 	data = {
@@ -13,3 +15,17 @@ def create_client(**kwargs):
 	data.update(kwargs)	
 	
 	return data
+
+
+def make_validated_form(**kwargs):
+    data = {
+		'name': 'Fellipe',
+		'email': 'email@email.com',
+		'adress': 'Street',
+		'telephone1': '123',
+		'telephone2': '321',
+    }
+    data.update(kwargs)
+    form = ClientForm(data)
+    form.is_valid()
+    return form
---
+++ estofadora/client/tests/test_forms.py
@@ -0,0 +1,34 @@
+# coding: utf-8
+
+from . import TestCase, make_validated_form, ClientForm
+
+
+class ClientFormTest(TestCase):
+
+	def test_if_has_fields(self):
+		form = ClientForm()
+		existing_fields = list(form.fields.keys())
+
+		expected_field = ['name', 'adress', 'email', 'telephone1', 'telephone2']
+
+		self.assertEqual(existing_fields, expected_field)
+
+	def test_email_is_optional(self):
+		form = make_validated_form(email='')
+		self.assertFalse(form.errors)
+
+	def test_telephone2_is_optional(self):
+		form = make_validated_form(telephone2='')
+		self.assertFalse(form.errors)
+
+	def test_name_is_not_optional(self):
+		form = make_validated_form(name='')
+		self.assertTrue(form.errors)
+
+	def test_telephone1_is_not_optional(self):
+		form = make_validated_form(telephone1='')
+		self.assertTrue(form.errors)
+
+	def test_form(self):
+		form = make_validated_form()
+		self.assertFalse(form.errors)
