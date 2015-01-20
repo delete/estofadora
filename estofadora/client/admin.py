f8cc42a6492fc9aeb1486f28c433a06258256ed7
delete <pinheiro.llip@gmail.com>
2015-01-20 11:20:54 -0200

    admin added to client

--- estofadora/client/admin.py
+++ estofadora/client/admin.py
@@ -1,3 +1,27 @@
+#coding: utf-8
+from django.utils.datetime_safe import datetime
 from django.contrib import admin
+from estofadora.client.models import Client
 
-# Register your models here.
+class ClientAdmin(admin.ModelAdmin):
+	list_display = ('name', 'adress', 'email', 'telephone1', 'is_active')
+	search_fields = ('name', 'adress', 'email', 'telephone1', 'telephone2')
+	date_hierarchy = 'date_join'
+	list_filter = ['date_join']
+
+	def subscribed_today(self, obj):
+		return obj.date_join.date() == datetime.today().date()
+
+	subscribed_today.short_description = (u'Cadsatrado hoje?')
+	subscribed_today.boolean = True
+
+	def mark_as_active(self, request, queryset):
+		count = queryset.update(is_active=True)
+
+		msg = u'%d clientes ativos.'
+
+		self.message_user(request, msg % count)
+
+	mark_as_active.short_description = ('Marcar como ativo')
+
+admin.site.register(Client, ClientAdmin)
