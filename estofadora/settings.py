b42e5a4b64c9c0d8724b1c61d076f42ad98f252f
delete <pinheiro.llip@gmail.com>
2015-10-15 23:40:29 -0300

    configs to heroku removed

--- estofadora/settings.py
+++ estofadora/settings.py
@@ -113,24 +113,24 @@ LOGIN_URL = 'login:login'
 LOGOUT_URL = 'login:logout'
 LOGIN_REDIRECT_URL = 'core:home'
 
-# HEROKU CONFIGS
+# # HEROKU CONFIGS
 
-# Parse database configuration from $DATABASE_URL
-import dj_database_url
-DATABASES['default'] =  dj_database_url.config()
+# # Parse database configuration from $DATABASE_URL
+# import dj_database_url
+# DATABASES['default'] =  dj_database_url.config()
 
-# Honor the 'X-Forwarded-Proto' header for request.is_secure()
-SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+# # Honor the 'X-Forwarded-Proto' header for request.is_secure()
+# SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
 
-# Allow all host headers
-ALLOWED_HOSTS = ['*']
+# # Allow all host headers
+# ALLOWED_HOSTS = ['*']
 
-# Static asset configuration
-BASE_DIR = os.path.dirname(os.path.abspath(__file__))
+# # Static asset configuration
+# BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static'),
-)
+# STATICFILES_DIRS = (
+#     os.path.join(BASE_DIR, 'static'),
+# )
 
 try:
     from estofadora.local_settings import *
