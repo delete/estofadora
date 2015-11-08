a63e0583597f492389984acc6ed1c0c430f8fda5
delete <pinheiro.llip@gmail.com>
2015-11-07 22:31:19 -0200

    wsgi updated to handle with media files

--- estofadora/wsgi.py
+++ estofadora/wsgi.py
@@ -11,7 +11,36 @@ import os
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "estofadora.settings")
 
 from django.core.wsgi import get_wsgi_application
-#application = get_wsgi_application()
 from dj_static import Cling
 
-application = Cling(get_wsgi_application())
+#application = Cling(get_wsgi_application())
+
+from django.conf import settings
+
+STATIC_ROOT = settings.STATIC_ROOT
+STATIC_URL = settings.STATIC_URL
+MEDIA_ROOT = settings.MEDIA_ROOT
+MEDIA_URL = settings.MEDIA_URL
+
+static_cling = Cling(STATIC_ROOT + '/')
+media_cling = Cling(MEDIA_ROOT + '/')
+
+
+class ServeStaticMiddleware(object):
+
+    def __init__(self, application):
+        self.application = application
+
+    def __call__(self, environ, start_response):
+        path = environ.get('PATH_INFO', '')
+        if path.startswith(STATIC_URL) and not settings.DEBUG:
+            environ['PATH_INFO'] = path.replace(STATIC_URL, '')
+            return static_cling(environ, start_response)
+        elif path.startswith(MEDIA_URL) and not settings.DEBUG:
+            environ['PATH_INFO'] = path.replace(MEDIA_URL, '')
+            return media_cling(environ, start_response)
+
+        return self.application(environ, start_response)
+
+
+application = ServeStaticMiddleware(get_wsgi_application())
