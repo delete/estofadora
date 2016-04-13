e8e1f73290101c38d4b1f5aa2fe6d035153b711f
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-04-13 14:07:07 -0300

    fix pep8 and remove api urls for now.

--- estofadora/urls.py
+++ estofadora/urls.py
@@ -1,8 +1,10 @@
-#coding: utf-8
+# coding: utf-8
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.conf import settings
 
-urlpatterns = patterns('',
+urlpatterns = patterns(
+    '',
     # Examples:
     # url(r'^$', 'estofadora.views.home', name='home'),
     # url(r'^blog/', include('blog.urls')),
@@ -10,15 +12,18 @@ urlpatterns = patterns('',
     url(r'^cliente/', include('estofadora.client.urls', namespace='client')),
     url(r'^item/', include('estofadora.item.urls', namespace='item')),
     url(r'^login/', include('estofadora.login.urls', namespace='login')),
-    url(r'^relatorios/', include('estofadora.statement.urls', namespace='statement')),
+    url(
+        r'^relatorios/',
+        include('estofadora.statement.urls', namespace='statement')
+    ),
     url(r'^contas/', include('estofadora.bills.urls', namespace='bills')),
-    url(r'^api/', include('estofadora.api.urls', namespace='api')),
     url(r'^admin/', include(admin.site.urls)),
+    # url(r'^api/', include('estofadora.api.urls', namespace='api')),
 )
 
-from django.conf import settings
+
 if settings.DEBUG:
-	from django.conf.urls.static import static
-	urlpatterns += static(
-		settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
-	)
+    from django.conf.urls.static import static
+    urlpatterns += static(
+        settings.MEDIA_URL, document_root=settings.MEDIA_ROOT
+    )
