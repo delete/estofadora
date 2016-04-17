50a4396ba50cb4f714797f2790196642a37b567a
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-04-17 18:32:09 -0300

    add some production settings

--- estofadora/settings/production.py
+++ estofadora/settings/production.py
@@ -1,8 +1,21 @@
 # -*- coding: utf-8 -*-
 from .base import *
 
+DEBUG = False
+
 OPBEAT = {
     'ORGANIZATION_ID': '6409f61f82ab4ee58bf78923b1645a29',
     'APP_ID': '8ee0dafc3e',
     'SECRET_TOKEN': 'bbedc0141783b774e7ba5061d7a8ed6328bc148e',
+}
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': '',
+        'USER': '',
+        'PASSWORD': '',
+        'HOST': 'localhost',
+        'PORT': '',
+    }
 }
