5027fea68a18c5f1819f780acc8bdf4ae895bdf1
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-05-09 22:41:21 -0300

    add some production settings

--- estofadora/settings/production.py
+++ estofadora/settings/production.py
@@ -3,6 +3,9 @@ from .base import *
 
 DEBUG = False
 SECRET_KEY = config('SECRET_KEY')
+ALLOWED_HOSTS = config(
+    'ALLOWED_HOSTS', cast=lambda v: [s.strip() for s in v.split(',')]
+)
 
 # OPBEAT
 # Uncomment with you want to use
