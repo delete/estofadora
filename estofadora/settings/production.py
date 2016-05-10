317f7b3848ecd2f3827db3d26cd3070edc840609
Fellipe Pinheiro <pinheiro.llip@gmail.com>
2016-05-10 00:05:06 -0300

    refacotored

--- estofadora/settings/production.py
+++ estofadora/settings/production.py
@@ -8,32 +8,22 @@ ALLOWED_HOSTS = config(
 )
 
 # OPBEAT
-# Uncomment with you want to use
-
-# ORGANIZATION_ID = config('ORGANIZATION_ID')
-# APP_ID = config('APP_ID')
-# SECRET_TOKEN = config('SECRET_TOKEN')
+# Uncomment if you want to use
 
 # OPBEAT = {
-#     'ORGANIZATION_ID': ORGANIZATION_ID,
-#     'APP_ID': APP_ID,
-#     'SECRET_TOKEN': SECRET_TOKEN,
+#     'ORGANIZATION_ID': config('ORGANIZATION_ID'),
+#     'APP_ID': config('APP_ID'),
+#     'SECRET_TOKEN': config('SECRET_TOKEN'),
 # }
 
 # DATABASE
-NAME = config('NAME')
-USER = config('USER')
-PASSWORD = config('PASSWORD')
-HOST = config('HOST')
-PORT = config('PORT')
-
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': NAME,
-        'USER': USER,
-        'PASSWORD': PASSWORD,
-        'HOST': HOST,
-        'PORT': PORT,
+        'NAME': config('NAME'),
+        'USER': config('USER'),
+        'PASSWORD': config('PASSWORD'),
+        'HOST': config('HOST'),
+        'PORT': config('PORT'),
     }
 }
